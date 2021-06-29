91a3905d46aa0e00d47009c6492258bfd2a98e97
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-06-29 16:02:46 -0300

    add routes principal, sobrenos and contato

---
+++ app/Http/Controllers/SobreNosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SobreNosController extends Controller
+{
+    public function sobrenos() {
+        return "chegamos na view sobre nós";
+    }
+}
