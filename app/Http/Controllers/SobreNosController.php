d5120c149daaa2a63100a54b403eb7c0c362a90a
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-06-29 18:32:12 -0300

    add views principal, sobrenos and contato

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class SobreNosController extends Controller
 {
     public function sobrenos() {
-        return "chegamos na view sobre nós";
+        return view('site.sobre-nos');
+
     }
 }
