f46ff075ba6ee208d18af0fb2a3b6757d12e5332
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-07-11 16:51:22 -0300

    add view create product

---
+++ resources/views/app/produto/create.blade.php
@@ -0,0 +1,34 @@
+@extends('app.layouts.basico')
+
+@section('title', 'Adicionar')
+
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Adicionar produto</p>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="{{route('produto.index')}}">Voltar</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+        <div class="informacao-pagina">
+            <div style="width: 30%; margin-left: auto; margin-right: auto">
+                <form action="" method="post">
+                    @csrf
+                    <input type="text" value="" name="nome" placeholder="Nome" class="borda-preta">
+                    <input type="text" value=""  name="descricao" placeholder="Descrição"  class="borda-preta">
+                    <input type="text" value=""  name="peso" placeholder="Peso"  class="borda-preta">
+                    <select name="unidade_id">
+                        <option value="">-- Selecione a Unidade de Medida --</option>
+                        @foreach ($unidades as $unidade)
+                            <option value="{{$unidade->id}}">{{$unidade->descricao}}</option>
+                        @endforeach
+                    </select>
+                    <button type="submit" class="borda-preta">Adicionar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
