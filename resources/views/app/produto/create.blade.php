6d02c3ada745063a7edb441e2475442259401af9
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-07-11 17:01:57 -0300

    add save new product in db - create method

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -15,7 +15,7 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto">
-                <form action="" method="post">
+                <form action="{{route('produto.store')}}" method="post">
                     @csrf
                     <input type="text" value="" name="nome" placeholder="Nome" class="borda-preta">
                     <input type="text" value=""  name="descricao" placeholder="Descrição"  class="borda-preta">
