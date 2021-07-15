0478fc108478bba498b68fba52af025eb4fb6367
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-07-15 12:20:47 -0300

    set relationship of pedido to produto

--- app/Http/Controllers/PedidoProdutoController.php
+++ app/Http/Controllers/PedidoProdutoController.php
@@ -27,7 +27,7 @@ class PedidoProdutoController extends Controller
     public function create(Pedido $pedido)
     {
         $produtos = Produto::all();
-        $pedido->produtos;
+        // $pedido->produtos;
         return view('app.pedido_produto.create', ['pedido' => $pedido, 'produtos' => $produtos]);
     }
 
